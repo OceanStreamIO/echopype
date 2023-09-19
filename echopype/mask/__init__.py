0b48e1a3b840cf313e407285ab199d333a1a2018
Ruxandra Valcu <ruxandra.valcu@gmail.com>
2023-09-19 18:30:28 +0300

    Merge/add seabed mask (#56)
    * Initial implementation of the seabed masking.
    
    * Code for seabed masking
    
    * Changed dilation source package from cv2 to skimage.
    
    * some refactoring and add unit test
    
    * Update echopype/mask/mask_seabed.py
    
    Co-authored-by: Raluca Simedroni <92971445+simedroniraluca@users.noreply.github.com>
    
    * Update echopype/tests/mask/test_mask_seabed.py
    
    Co-authored-by: Raluca Simedroni <92971445+simedroniraluca@users.noreply.github.com>
    
    * Re-added mask_transformation, with only the necessary functions kept
    
    * Changed the tests to be able to autodownload its files - must check that I have the correct one though since the test isn't passing
    
    * Cleaned up mask_transformation
    
    * Copyright notices
    
    Co-authored-by: Raluca Simedroni <92971445+simedroniraluca@users.noreply.github.com>
    
    * Update echopype/mask/mask_seabed.py
    
    Co-authored-by: Raluca Simedroni <92971445+simedroniraluca@users.noreply.github.com>
    
    * Update echopype/mask/mask_seabed.py
    
    Co-authored-by: Raluca Simedroni <92971445+simedroniraluca@users.noreply.github.com>
    
    * Update incorrect file name
    
    * Mask transformation consistency
    
    * Mask transformation consistency
    
    * Removing pytest req
    
    * Refactoring
    
    * Fix twod test
    
    * Refactoring
    
    * Test fixes
    
    * Fixing merge compatibility
    
    * Added dataset generation for datasets enriched with beam angle and location data
    
    * Added dataset generation for datasets enriched with beam angle and location data
    
    * Added channel selection.
    
    * Modified Blackwell methods to use the physical beam angle rather than the raw EK60 beam angle - doing this using a correction on the angle itself (rather than modifying the constants) since the changes are nonlinear so if I want a linear transformation, I need to apply it on the angle data itself - angle * 22 * 128 / 180, which gets it close to how a decently calibrated EK60 instrument as used by Blackwell et al stores its raw angle data.
    
    * Indent fix
    
    * Merge fixes
    
    ---------
    
    Co-authored-by: Mihai Boldeanu <mishu_boldy@yahoo.com>
    Co-authored-by: Andrei Rusu <andrei.rusu@beatfactor.net>
    Co-authored-by: Andrei Rusu <beatfactor@users.noreply.github.com>
    Co-authored-by: Raluca Simedroni <92971445+simedroniraluca@users.noreply.github.com>

--- echopype/mask/__init__.py
+++ echopype/mask/__init__.py
@@ -1,3 +1,3 @@
-from .api import apply_mask, frequency_differencing, shoal_weill
+from .api import apply_mask, frequency_differencing, get_seabed_mask, shoal_weill
 
-__all__ = ["frequency_differencing", "apply_mask", "shoal_weill"]
+__all__ = ["frequency_differencing", "apply_mask", "get_seabed_mask", "shoal_weill"]
