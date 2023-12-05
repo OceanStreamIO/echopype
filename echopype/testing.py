4617a29cd743aea58c7ce49cd83fabf1e929a478
Mihai Boldeanu <mishu_boldy@yahoo.com>
2023-12-05 12:31:42 +0200

    Fixed typo

--- echopype/testing.py
+++ echopype/testing.py
@@ -5,4 +5,5 @@ from pathlib import Path
 # TEST_DATA_FOLDER = HERE / "test_data"
 current_directory = os.path.dirname(os.path.abspath(__file__))
 TEST_DATA_FOLDER = os.path.join(current_directory, "..", "test_data")
+
 TEST_DATA_FOLDER = Path(TEST_DATA_FOLDER)
