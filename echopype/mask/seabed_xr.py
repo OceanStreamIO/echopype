42d01837608ae7d43a98aa34e6a634d727343163
ruxandra valcu <ruxandra.valcu@gmail.com>
2023-12-05 12:28:06 +0200

    Functional blackwell

--- echopype/mask/seabed_xr.py
+++ echopype/mask/seabed_xr.py
@@ -35,10 +35,14 @@ import warnings
 
 import dask.array as da
 import numpy as np
+import scipy.ndimage as nd_img
 import xarray as xr
 from dask_image.ndfilters import convolve
+from dask_image.ndmeasure import label
 from dask_image.ndmorph import binary_dilation, binary_erosion
+from scipy.signal import convolve2d
 
+from ..utils.mask_transformation import lin, log
 from ..utils.mask_transformation_xr import line_to_square
 
 MAX_SV_DEFAULT_PARAMS = {"r0": 10, "r1": 1000, "roff": 0, "thr": (-40, -60)}
@@ -458,49 +462,190 @@ def _blackwell(Sv_ds: xr.DataArray, desired_channel: str, parameters: dict = MAX
             + ", are: "
             + str(parameters.keys())
         )
-    # r0 = parameters["r0"]
-    # r1 = parameters["r1"]
-    # tSv = parameters["tSv"]
+    r0 = parameters["r0"]
+    r1 = parameters["r1"]
+    tSv = parameters["tSv"]
     ttheta = parameters["ttheta"]
     tphi = parameters["tphi"]
     wtheta = parameters["wtheta"]
     wphi = parameters["wphi"]
 
     channel_Sv = Sv_ds.sel(channel=desired_channel)
-    # Sv = channel_Sv["Sv"]
-    print(channel_Sv["angle_alongship"].max().item())
+    Sv = channel_Sv["Sv"]
+    r = channel_Sv["echo_range"][0]
     theta = channel_Sv["angle_alongship"].copy() * 22 * 128 / 180
-    print(theta)
-    print(theta.max())
     phi = channel_Sv["angle_athwartship"].copy() * 22 * 128 / 180
 
     dask_theta = da.asarray(theta, allow_unknown_chunksizes=False)
     dask_theta.compute_chunk_sizes()
-    theta.values = convolve(
-        dask_theta,
-        weights=da.ones(shape=(wtheta, wtheta), dtype=float) / wtheta**2,
-        mode="nearest",
-    ).compute()
+    theta.values = (
+        convolve(
+            dask_theta,
+            weights=da.ones(shape=(wtheta, wtheta), dtype=float) / wtheta**2,
+            mode="nearest",
+        )
+        ** 2
+    )
 
     dask_phi = da.asarray(phi, allow_unknown_chunksizes=False)
     dask_phi.compute_chunk_sizes()
-    phi.values = convolve(
-        dask_phi,
-        weights=da.ones(shape=(wphi, wphi), dtype=float) / wphi**2,
-        mode="nearest",
-    ).compute()
+    phi.values = (
+        convolve(
+            dask_phi,
+            weights=da.ones(shape=(wphi, wphi), dtype=float) / wphi**2,
+            mode="nearest",
+        )
+        ** 2
+    )
 
-    angle_mask = ~((theta > ttheta) | (phi > tphi))
+    angle_mask = ~((theta > ttheta) | (phi > tphi)).compute()
 
     if angle_mask.all():
         warnings.warn(
             "No aliased seabed detected in Theta & Phi. "
             "A default mask with all True values is returned."
         )
-        print("Oof")
         return angle_mask
+    # negate for further processing
+    angle_mask = ~angle_mask
 
-    mask = theta > ttheta
-    print(theta)
+    # calculate median Sv of angle-masked regions, and mask Sv above
+    Sv_masked = Sv.where(angle_mask)
+    Sv_median_anglemasked = Sv_masked.median(skipna=True).item()
 
+    if np.isnan(Sv_median_anglemasked):
+        Sv_median_anglemasked = np.inf
+    if Sv_median_anglemasked < tSv:
+        Sv_median_anglemasked = tSv
+    Sv_threshold_mask = Sv > Sv_median_anglemasked
+
+    # create structure element that defines connections
+    structure = da.ones(shape=(3, 3), dtype=bool)
+    items = label(Sv_threshold_mask, structure)[0]
+
+    items_data = xr.DataArray(
+        items,
+        dims=angle_mask.dims,
+        coords=angle_mask.coords,
+    )
+
+    mask_items = items_data.where(angle_mask, 0)
+
+    # get items intercepted by angle mask
+    keep_items = np.unique(mask_items.values)
+    keep_items = keep_items[keep_items > 0]
+    angle_items = xr.where(items_data.isin(keep_items), items_data, 0)
+    angle_items_mask = ~(angle_items > 0)
+
+    mask = angle_items_mask
+
+    # apply range filter
+    # get upper and lower range indexes
+    up = abs(r - r0).argmin(dim="range_sample").item()
+    lw = abs(r - r1).argmin(dim="range_sample").item()
+
+    # get threshold mask with shallow and deep waters masked
+    range_filter = (mask["range_sample"] >= up) & (mask["range_sample"] <= lw)
+    mask = mask.where(range_filter, other=True)
     return mask
+
+
+def _blackwell_old(
+    Sv_ds: xr.DataArray, desired_channel: str, parameters: dict = BLACKWELL_DEFAULT_PARAMS
+):
+    parameter_names = ["r0", "r1", "tSv", "ttheta", "tphi", "wtheta", "wphi"]
+    if not all(name in parameters.keys() for name in parameter_names):
+        raise ValueError(
+            "Missing parameters - should be: "
+            + str(parameter_names)
+            + ", are: "
+            + str(parameters.keys())
+        )
+    r0 = parameters["r0"]
+    r1 = parameters["r1"]
+    tSv = parameters["tSv"]
+    ttheta = parameters["ttheta"]
+    tphi = parameters["tphi"]
+    wtheta = parameters["wtheta"]
+    wphi = parameters["wphi"]
+
+    channel_Sv = Sv_ds.sel(channel=desired_channel)
+    Sv = channel_Sv["Sv"].values.T
+    r = Sv_ds["echo_range"].values[0, 0]
+    theta = channel_Sv["angle_alongship"].values.T
+    phi = channel_Sv["angle_athwartship"].values.T
+    # apply reverse correction on theta & phi to match Blackwell's constants
+    theta = theta * 22 * 128 / 180
+    phi = phi * 22 * 128 / 180
+
+    # delimit the analysis within user-defined range limits
+    r0 = np.nanargmin(abs(r - r0))
+    r1 = np.nanargmin(abs(r - r1)) + 1
+    Svchunk = Sv[r0:r1, :]
+    thetachunk = theta[r0:r1, :]
+    phichunk = phi[r0:r1, :]
+
+    # get blur kernels with theta & phi width dimensions
+    ktheta = np.ones((wtheta, wtheta)) / wtheta**2
+    kphi = np.ones((wphi, wphi)) / wphi**2
+
+    # perform mean square convolution and mask if above theta & phi thresholds
+    thetamaskchunk = convolve2d(thetachunk, ktheta, "same", boundary="symm") ** 2 > ttheta
+    phimaskchunk = convolve2d(phichunk, kphi, "same", boundary="symm") ** 2 > tphi
+    anglemaskchunk = thetamaskchunk | phimaskchunk
+
+    if True:
+        above = np.zeros((r0, Svchunk.shape[1]), dtype=bool)
+        below = np.zeros((len(r) - r1, Svchunk.shape[1]), dtype=bool)
+        mask = np.r_[above, anglemaskchunk, below]
+
+        mask = np.logical_not(mask.T)
+        return_mask = xr.DataArray(
+            mask,
+            dims=("ping_time", "range_sample"),
+            coords={"ping_time": channel_Sv.ping_time, "range_sample": channel_Sv.range_sample},
+        )
+        return return_mask
+
+    # if aliased seabed, mask Sv above the Sv median of angle-masked regions
+    if anglemaskchunk.any():
+        Svmedian_anglemasked = log(np.nanmedian(lin(Svchunk[anglemaskchunk])))
+        if np.isnan(Svmedian_anglemasked):
+            Svmedian_anglemasked = np.inf
+        if Svmedian_anglemasked < tSv:
+            Svmedian_anglemasked = tSv
+        Svmaskchunk = Svchunk > Svmedian_anglemasked
+
+        # label connected items in Sv mask
+        items = nd_img.label(Svmaskchunk, nd_img.generate_binary_structure(2, 2))[0]
+
+        # get items intercepted by angle mask (likely, the seabed)
+        intercepted = list(set(items[anglemaskchunk]))
+        if 0 in intercepted:
+            intercepted.remove(intercepted == 0)
+
+        # combine angle-intercepted items in a single mask
+        maskchunk = np.zeros(Svchunk.shape, dtype=bool)
+        for i in intercepted:
+            maskchunk = maskchunk | (items == i)
+
+        # add data above r0 and below r1 (removed in first step)
+        above = np.zeros((r0, maskchunk.shape[1]), dtype=bool)
+        below = np.zeros((len(r) - r1, maskchunk.shape[1]), dtype=bool)
+        mask = np.r_[above, maskchunk, below]
+
+    # give empty mask if aliased-seabed was not detected in Theta & Phi
+    else:
+        warnings.warn(
+            "No aliased seabed detected in Theta & Phi. "
+            "A default mask with all True values is returned."
+        )
+        mask = np.zeros_like(Sv, dtype=bool)
+
+    mask = np.logical_not(mask.T)
+    return_mask = xr.DataArray(
+        mask,
+        dims=("ping_time", "range_sample"),
+        coords={"ping_time": channel_Sv.ping_time, "range_sample": channel_Sv.range_sample},
+    )
+    return return_mask
